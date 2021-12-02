d93930fbb9996f21d5553589faf2766286d51a56
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2021-12-02 07:15:13 +0000

    code can look for words in array

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,22 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
+    if (letters.length < 1 || word.length < 1) return false;
     const horizontalJoin = letters.map(ls => ls.join(''))
+    console.log("horizontal join", horizontalJoin)
+    const vertialJoin = letters[0].map((col, index) =>
+        letters.map((row) => row[index])
+    );
+    const verticalFlip = vertialJoin.map((ls) => ls.join(""));
+    console.log(verticalFlip)
+    vertialJoin.map((value) => console.log("value", value));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    if (l.includes(word)) return true;
+    else {
+        for (l of verticalFlip) {
+            if (l.includes(word)) return true;
+            }
+        }
     }
-}
+    return false;
+};
 
 module.exports = wordSearch
